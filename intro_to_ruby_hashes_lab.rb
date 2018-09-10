fce8e1f23e7aff62fa4690f5382f06686facb7ed
Shuo Wang <gken90@gmail.com>
2018-09-10 21:38:12 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -69,10 +69,10 @@ def monopoly_with_fourth_tier
 					"mortgage_value" => "$200",
 				},
 				:b_and_o_railroad => {
-
+					"mortgage_value" => "$400",
 				},
 				:shortline => {
-
+					"mortgage_value" => "$800",
 				},
 
 		},
